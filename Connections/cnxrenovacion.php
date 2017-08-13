9023476e27c92c951283e621541de96b7a10e22b
lsiancas <30275035+lsiancas@users.noreply.github.com>
2017-08-13 15:07:29 -0500

    Update cnxrenovacion.php

--- Connections/cnxrenovacion.php
+++ Connections/cnxrenovacion.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_cnxrenovacion = "localhost";
-$database_cnxrenovacion = "renovacion";
-$username_cnxrenovacion = "root";
-$password_cnxrenovacion = "201504";
+$hostname_cnxrenovacion = "us-cdbr-sl-dfw-01.cleardb.net";
+$database_cnxrenovacion = "ibmx_2b007a5fdcd35e5";
+$username_cnxrenovacion = "bd8942ac845bbb";
+$password_cnxrenovacion = "f53a99b3";
 $cnxrenovacion = mysql_pconnect($hostname_cnxrenovacion, $username_cnxrenovacion, $password_cnxrenovacion) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
