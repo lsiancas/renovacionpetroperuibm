8dd95fc072ccc9e459054acb10c82c108cca4803
lsiancas <30275035+lsiancas@users.noreply.github.com>
2017-08-15 19:54:23 -0500

    Update devolucion.php

--- devolucion.php
+++ devolucion.php
@@ -97,7 +97,7 @@ $update = "update equiposold set cambio = 'REALIZADO' where activoold = '$regist
 mysql_query($update, $cnxrenovacion) or die ("Error $update".mysql_error());
  
  /*Seccion Creacion Plantilla*/
-require_once dirname(__FILE__).'/PHPWord-master/src/PhpWord/Autoloader.php';
+include_once dirname(__FILE__).'/PHPWord-master/src/PhpWord/Autoloader.php';
 \PhpOffice\PhpWord\Autoloader::register();
 
 use PhpOffice\PhpWord\TemplateProcessor;
